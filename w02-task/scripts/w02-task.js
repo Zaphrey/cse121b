a2f09c0c16c44c6e23b3783a68430876ddf1a321
Zachary Humphreys <zachary.humphreys32@gmail.com>
2024-03-06 16:04:18 -0500

    completed w02 midweek programming task

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,37 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+
+let fullName = "Zachary Humphreys";
+let currentYear = "2023";
+let profilePicture = "images/me_with_friends.jpg";
+
+/* Step 3 - Element Variables */
+
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const imageElement = document.querySelector("img");
+
+/* Step 4 - Adding Content */
+
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute("src", profilePicture);
+
+/* Step 5 - Array */
+
+let favoriteFoods = ["Lasagna", "Chicken Enchiladas", "Sausage Rice", "Caesar Salad"];
+foodElement.innerHTML = `<strong>${favoriteFoods}</strong>`;
+
+// Declare and append tomato soup to food element
+let favoriteFood = "Tomato Soup";
+favoriteFoods.push(favoriteFood);
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+
+// Remove first element from favorite food array
+// and append the same array to the food element
+favoriteFoods.shift();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
